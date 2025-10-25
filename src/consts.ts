d152d9f8c347ae1c0780f4567880eedb0ffcdc06
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-10-25 18:15:36 +0200

    feat: add support for efr32mg24a420f1536im40

--- src/consts.ts
+++ src/consts.ts
@@ -124,6 +124,50 @@ export const CHIP_EFR32MG24A020F1024IM40: ChipInfo = {
     userdataBits: 0xb,
 };
 
+const EFR32MG24A420F1536IM40_FLASH_BASE = 0x08000000;
+
+/**
+ * platform/Device/SiliconLabs/EFR32MG24/Include/efr32mg24a420f1536im40.h
+ */
+export const CHIP_EFR32MG24A420F1536IM40: ChipInfo = {
+    chip: "EFR32MG24A420F1536IM40",
+
+    flashBase: EFR32MG24A420F1536IM40_FLASH_BASE,
+    flashSize: 0x00180000,
+    flashPageSize: 0x00002000,
+
+    btlApplicationBase: EFR32MG24A420F1536IM40_FLASH_BASE + 0x00006000,
+
+    nvm3DefaultNVMSizes: [NVM3_DEFAULT_NVM_SIZE, 32768],
+
+    userdataBase: 0x0fe00000,
+    userdataSize: 0x00000400,
+    userdataEnd: 0x0fe003ff,
+    userdataBits: 0xb,
+};
+
+const EFR32MG24B220F1536IM48_FLASH_BASE = 0x08000000;
+
+/**
+ * platform/Device/SiliconLabs/EFR32MG24/Include/efr32mg24b220f1536im48.h
+ */
+export const CHIP_EFR32MG24B220F1536IM48: ChipInfo = {
+    chip: "EFR32MG24B220F1536IM48",
+
+    flashBase: EFR32MG24B220F1536IM48_FLASH_BASE,
+    flashSize: 0x00180000,
+    flashPageSize: 0x00002000,
+
+    btlApplicationBase: EFR32MG24B220F1536IM48_FLASH_BASE + 0x00006000,
+
+    nvm3DefaultNVMSizes: [NVM3_DEFAULT_NVM_SIZE, 32768],
+
+    userdataBase: 0x0fe00000,
+    userdataSize: 0x00000400,
+    userdataEnd: 0x0fe003ff,
+    userdataBits: 0xb,
+};
+
 export const MGM240PA32VNN_FLASH_BASE = 0x08000000;
 
 /**
@@ -181,28 +225,6 @@ export const CHIP_MGM240PB32VNA: ChipInfo = {
     flashPageSize: 0x00002000,
 
     btlApplicationBase: MGM240PB32VNA_FLASH_BASE + 0x00006000,
-
-    nvm3DefaultNVMSizes: [NVM3_DEFAULT_NVM_SIZE, 32768],
-
-    userdataBase: 0x0fe00000,
-    userdataSize: 0x00000400,
-    userdataEnd: 0x0fe003ff,
-    userdataBits: 0xb,
-};
-
-const EFR32MG24B220F1536IM48_FLASH_BASE = 0x08000000;
-
-/**
- * platform/Device/SiliconLabs/EFR32MG24/Include/efr32mg24b220f1536im48.h
- */
-export const CHIP_EFR32MG24B220F1536IM48: ChipInfo = {
-    chip: "EFR32MG24B220F1536IM48",
-
-    flashBase: EFR32MG24B220F1536IM48_FLASH_BASE,
-    flashSize: 0x00180000,
-    flashPageSize: 0x00002000,
-
-    btlApplicationBase: EFR32MG24B220F1536IM48_FLASH_BASE + 0x00006000,
 
     nvm3DefaultNVMSizes: [NVM3_DEFAULT_NVM_SIZE, 32768],
 
